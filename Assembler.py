69f4de83c4ccb8bbb5b978141352462424f223e6
Chris Springstead <chrisspringstead@Chriss-MacBook-Pro-2.local>
2019-04-03 08:51:11 -0500

    can read in asm file given path from command line

---
+++ Assembler.py
@@ -0,0 +1,27 @@
+# Project 6 Assembler written in Python3
+
+import sys
+
+def main():
+	#getting argument from command line
+	path_to_file = sys.argv[1]
+	input_file = open(path_to_file, 'r')
+	for line in input_file:
+		print(line)
+
+	# symbol tabel which will hold values for labels and variables 
+	symbol_tabel = {"R0" : 0, "R1": 1, "R2" : 2} 
+
+	# first pass used for finding labels
+	'''
+	def firstPass():
+
+	# second pass used for reading asm code
+	def secondPass():
+
+	def main():
+		firstPass()
+		secondPass()
+	'''	
+if __name__ == '__main__':
+	main()
